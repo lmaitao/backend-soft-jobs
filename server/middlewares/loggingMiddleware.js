28226ca3d2df3d4d4df7f5b4e83af0ed35925662
Luis Maita <lmaitao25@gmail.com>
2025-07-03 22:24:47 -0400

    error

--- server/middlewares/loggingMiddleware.js
+++
@@ -1,7 +0,0 @@
-export default (req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`, {
-    body: req.body,
-    headers: req.headers
-  });
-  next();
-};
